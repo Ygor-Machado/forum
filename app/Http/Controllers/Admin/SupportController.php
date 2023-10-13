9aa835e8146d28516c6f3fdd0fbe87834e0149b1
Ygor-Machado <ygaounderground011@gmail.com>
2023-10-13 19:58:07 -0300

    Deletar registros

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -59,4 +59,15 @@ class SupportController extends Controller
         return redirect()->route('supports.index');
     }
 
+    public function destroy(Support $support, $id)
+    {
+        if (!$support = Support::find($id)){
+            return back();
+        }
+
+        $support->delete();
+
+        return redirect()->route('supports.index');
+    }
+
 }
