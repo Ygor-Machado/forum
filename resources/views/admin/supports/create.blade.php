4125e7ce177ac21c496fe2de1a30328ea5eb6475
Ygor-Machado <ygaounderground011@gmail.com>
2023-10-14 02:56:51 -0300

    Validações

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,8 +1,14 @@
 <h1>Nova Dúvida</h1>
 
+@if($errors->any())
+    @foreach($errors->all() as $error)
+        <p>{{$error}}</p>
+    @endforeach
+@endif
+
 <form action="{{route('supports.store')}}" method="POST">
     @csrf
-    <input type="text" placeholder="Assunto" name="subject">
-    <textarea name="body" cols="30" row="5" placeholder="Descrição"></textarea>
+    <input type="text" placeholder="Assunto" name="subject" value="{{ old('subject') }}">
+    <textarea name="body" cols="30" row="5" placeholder="Descrição">{{ old('body') }}</textarea>
     <button type="submit">Enviar</button>
 </form>
