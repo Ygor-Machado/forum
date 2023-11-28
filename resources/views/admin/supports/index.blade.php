fe40d0d1ce87d816c9ef2a10c52c690d0f6ae411
Ygor-Machado <ygaounderground011@gmail.com>
2023-11-28 03:55:23 -0300

    Utilizando tailwind na aplicação

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,32 +1,17 @@
-<h1>Listagem dos suportes</h1>
+@extends('admin.layouts.app')
 
-<a href="{{route('supports.create')}}">Criar Dúvida</a>
+@section('title', 'Fórum')
 
-<table>
-    <thead>
-        <th>assunto</th>
-        <th>status</th>
-        <th>descrição</th>
-        <th></th>
-    </thead>
+@section('header')
+    @include('admin.supports.partials.header')
+@endsection
 
-    <tbody>
-        @foreach($supports->items() as $support)
-            <tr>
-                <td>{{ $support->subject }}</td>
-                <td>{{ \App\Helpers\getStatusSupport($support->status) }}</td>
-                <td>{{ $support->body }}</td>
-                <td>
-                    <a href="{{route('supports.show', $support->id) }}"> > </a>
-                    <a href="{{route('supports.edit', $support->id) }}">Editar</a>
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+@section('content')
+    @include('admin.supports.partials.content')
 
 <x-pagination
               :paginator="$supports"
               :appends="$filters"
 />
+@endsection
 
