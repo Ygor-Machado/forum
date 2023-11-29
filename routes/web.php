cd81efb24eefeb6cc0296e745162b15f77e86004
Ygor-Machado <ygaounderground011@gmail.com>
2023-11-29 04:00:04 -0300

    refatorando view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ReplySupportController;
 use App\Http\Controllers\Admin\SupportController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -16,11 +17,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/supports/{id}/replies', [ReplySupportController::class, 'index'])->name('replies.index');
     Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
     Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
     Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
     Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
-    Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
     Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
     Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 });
