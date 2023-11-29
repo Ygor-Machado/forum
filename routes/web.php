6bc3d3704afd7bdc30c75a620052127b3d824c06
Ygor-Machado <ygaounderground011@gmail.com>
2023-11-29 02:08:45 -0300

    Autenticação Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
 use App\Http\Controllers\Admin\SupportController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
-Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
-Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
-Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
-Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
-Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
-Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+R
 
 Route::get('/contato', [SiteController::class, 'contact']);
 
@@ -18,3 +13,23 @@ Route::get('/contato', [SiteController::class, 'contact']);
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
+    Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
+    Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
+    Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+    Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
+    Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
+    Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+});
+
+require __DIR__.'/auth.php';
+
