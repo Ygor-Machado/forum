c7a2119cffcf1f410f7b6602c24a078118a64524
Ygor-Machado <ygaounderground011@gmail.com>
2023-11-29 20:03:45 -0300

    Criando Observer

---
+++ app/Observers/SupportObserver.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Support;
+use Illuminate\Support\Facades\Auth;
+
+class SupportObserver
+{
+    /**
+     * Handle the Support "created" event.
+     */
+    public function creating(Support $support): void
+    {
+        $support->user_id = Auth::user()->id;
+    }
+
+
+}
