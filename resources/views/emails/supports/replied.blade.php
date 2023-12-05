47f30189fe93dd6319ee379945e559e35e7ad2bd
Ygor-Machado <ygaounderground011@gmail.com>
2023-12-05 15:45:13 -0300

    altera view email

--- resources/views/emails/supports/replied.blade.php
+++ resources/views/emails/supports/replied.blade.php
@@ -1,12 +1,12 @@
 <x-mail::message>
 # Dúvida Respondida
 
-Assunto da dúvida {{ $reply->support_id }} foi respondi com <b>{{ $reply->content }}</b>.
+    Assunto da dúvida {{ $reply->support_id }} foi respondi com <b>{{ $reply->content }}</b>.
 
 <x-mail::button :url="route('replies.index', $reply->support_id)">
-Ver
+    Ver
 </x-mail::button>
 
 Obrigado,<br>
-{{ config('app.name') }}
+    {{ config('app.name') }}
 </x-mail::message>
